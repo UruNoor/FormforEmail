60e215cd4e56c09e0873ee72e2ce9d8ad7c17ca1
UruNoor <noorhai.flower@gmail.com>
2025-03-14 07:00:52 +0300

    Update submit_complaint.php

--- submit_complaint.php
+++ submit_complaint.php
@@ -1,16 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $complaint = htmlspecialchars($_POST['complaint']);
 
-    // Save the complaint to a file (or database)
-    $file = 'complaints.txt';
-    $data = "Name: $name\nEmail: $email\nComplaint: $complaint\n\n";
-    file_put_contents($file, $data, FILE_APPEND);
-
-    // Redirect back to the form
-    header("Location: index.html");
-    exit();
-}
-?>
