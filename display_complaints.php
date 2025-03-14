45b151da50220b78b033b77ab06728c2d57098a9
UruNoor <noorhai.flower@gmail.com>
2025-03-14 06:59:45 +0300

    Update display_complaints.php

--- display_complaints.php
+++ display_complaints.php
@@ -1,9 +1,1 @@
-<?php
-$file = 'complaints.txt';
-if (file_exists($file)) {
-    $complaints = file_get_contents($file);
-    echo "<pre>$complaints</pre>";
-} else {
-    echo "No complaints yet.";
-}
-?>
+
